dad8e8d7fb03fb9c8da2f109487518fe0bcf8ecb
marcosprofile <marcos_laurindo1997@hotmail.com>
2023-08-11 19:34:13 -0300

    update new project

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -9,6 +9,7 @@ import JornadaMilhas from '../assets/jornada_milhas.png';
 import MyTodoList from '../assets/my_todo_list.png';
 import FormStep from '../assets/form_step.png';
 import ViaCEP from '../assets/consulta_cep.png';
+import Gooday from '../assets/gooday.png';
 import PrimeFlix from '../assets/prime_flix.png';
 import Finans from '../assets/finans.png';
 import Spotify from '../assets/spotify.png';
@@ -75,30 +76,39 @@ export const projectDetails = [
   },
   {
     id: 4,
+    project_name: 'Gooday',
+    project_desc: 'Neste projeto de Figma, alcançei um domínio completo da ferramenta, adquirindo uma compreensão profunda do design de interface de usuário e suas tecnologias. Na criação deste aplicativo de rede social, desenvolvi habilidades para atuar como UI Designer em equipes de alto desempenho, aprendi a prototipar com qualidade de mercado, otimizei a eficiência na criação com recursos como bibliotecas e cores, ganhei expertise em exportar projetos de forma profissional para facilitar a colaboração com programadores. Em suma, me tornei um designer versátil, inovador e apto a liderar projetos de design de aplicativos e sites.',
+    tech_stack: ['Figma'],
+    project_img: Gooday,
+    project_url: 'https://www.behance.net/gallery/174834333/Gooday',
+    reverse: false,
+  },
+  {
+    id: 5,
     project_name: 'Prime Flix',
     project_desc: 'Este projeto foi realizado com React, realizando a consulta de uma API externa para a construção da aplicação. O intuito é listar os filmes na página inicial, trazer a descrição do filme na tela de detalhes possibilitando o usuário de salvá-lo como favorito ou visualizar o trailer no youtuve e listar os filmes favoritos também possibilitando de remover da lista. Também tem uma tela de erro caso o usuário insira uma URL inválida.',
     tech_stack: ['React JS', 'CSS', 'TypeScript'],
     project_img: PrimeFlix,
     project_url: 'https://prime-flix-one.vercel.app/',
-    reverse: false,
+    reverse: true,
   },
   {
-    id: 5,
+    id: 6,
     project_name: 'Finans',
     project_desc: 'Este projeto foi realizado no meu início de carreira com bootstrap. Construi a interface de uma landing page de forma rápida com componentes e classes do bootstrap, onde foi possivel deixar a interface responsiva sem a necessidade de utilizar muitas linhas de código no CSS para obter o mesmo resultado.',
     tech_stack: ['HTML', 'Bootstrap', 'CSS'],
     project_img: Finans,
     project_url: 'https://projeto-finans-iota.vercel.app/',
-    reverse: true,
+    reverse: false,
   },
   {
-    id: 6,
+    id: 7,
     project_name: 'Spotify',
     project_desc: 'Este projeto foi realizado no meu início de carreira com bootstrap. Contrui a interface de uma landing page de forma rápida com componentes e classes do bootstrap, onde foi possivel deixar a interface responsiva sem a necessidade de utilizar muitas linhas de código no CSS para obter o mesmo resultado. ',
     tech_stack: ['HTML', 'Bootstrap', 'CSS'],
     project_img: Spotify,
     project_url: 'https://spotify-wheat-six.vercel.app/',
-    reverse: false,
+    reverse: true,
   },
 ];
 
