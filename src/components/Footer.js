5c08b26d22f0334789b85016d9afa5023bd1c410
marcosprofile <marcos_laurindo1997@hotmail.com>
2025-04-12 22:54:20 -0300

    feat: remove padding left and right from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import { PaddingContainer, Heading } from '../styles/Global.styled';
 const Footer = () => {
   return (
     <footer>
-      <PaddingContainer top="2.5rem" bottom="2.5rem" left="1rem" right="1rem">
+      <PaddingContainer top="2.5rem" bottom="2.5rem">
         <Heading as="p" size="p" align="center">&copy; 2025 | Marcos L. Ferreira • UI/UX Developer - Todos os direitos reservados.</Heading>
       </PaddingContainer>
     </footer>
