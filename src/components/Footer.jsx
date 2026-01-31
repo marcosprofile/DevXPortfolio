33f6b464b1523af32ed87dc48e02d10ef850af60
marcosprofile <marcos_laurindo1997@hotmail.com>
2026-01-31 18:42:08 -0300

    feat: update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <FooterContainer>
       <PaddingContainer $top="1.5rem" $bottom="1.5rem">
         <Heading as="p" size="p" $align="center">
-          &copy; {new Date().getFullYear()} | Marcos L. Ferreira • UX/UI Developer - Todos os direitos reservados.
+          &copy; {new Date().getFullYear()} | Marcos L. Ferreira • Front-end Engineer - Todos os direitos reservados.
         </Heading>
       </PaddingContainer>
     </FooterContainer>
